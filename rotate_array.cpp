da41fbb4d9ee3ae7481a672f7857f44a21ee0da5
Rahul35500 <60893884+Rahul35500@users.noreply.github.com>
2020-11-12 14:26:07 +0530

    Add files via upload

---
+++ rotate_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int arr[n];
+    cout<<"Enter the array of the size which you want:";
+    cin>>n;
+    cout<<"Enter the array:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    return 0;
+}
